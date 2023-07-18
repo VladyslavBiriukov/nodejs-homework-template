0a6a5257366dd7e72b91d8bdb13e985d86cc9fdd
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-07-18 20:15:02 +0300

    move db host

---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = "mongodb+srv://Vlad:WJEOlDGfatxPYNlW@cluster0.t1uvruu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+module.exports = {
+    DB_HOST,
+};
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Vlad:WJEOlDGfatxPYNlW@cluster0.t1uvruu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set('strictQuery', true);
 
